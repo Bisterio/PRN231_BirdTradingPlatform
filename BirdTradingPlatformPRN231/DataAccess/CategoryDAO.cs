c0c5b6e38f5327a57a2d7d7d4bfd43803122eb78
cris nguyen <kingericvt96@gmail.com>
2023-06-19 14:31:36 +0700

    CRUD ProductDAO, CategoryDAO

--- BirdTradingPlatformPRN231/DataAccess/CategoryDAO.cs
+++ BirdTradingPlatformPRN231/DataAccess/CategoryDAO.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BusinessObject.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,84 @@ namespace DataAccess
 {
     public class CategoryDAO
     {
+        public static List<Category> GetCategories()
+        {
+            var listCategorys = new List<Category>();
+            try
+            {
+                using (var context = new BirdTradingPlatformContext())
+                {
+                    listCategorys = context.Categories.ToList();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            return listCategorys;
+        }
+        public static Category FindCategoryById(int CategoryId)
+        {
+            Category f = new Category();
+            try
+            {
+                using (var context = new BirdTradingPlatformContext())
+                {
+                    f = context.Categories
+                        .SingleOrDefault(x => x.CategoryId == CategoryId);
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            return f;
+        }
+        public static void SaveCategory(Category Category)
+        {
+            try
+            {
+                using (var context = new BirdTradingPlatformContext())
+                {
+                    context.Categories.Add(Category);
+                    context.SaveChanges();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+        public static void UpdateCategory(Category Category)
+        {
+            try
+            {
+                using (var context = new BirdTradingPlatformContext())
+                {
+                    context.Entry<Category>(Category).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+                    context.SaveChanges();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+        public static void DeleteCategory(Category Category)
+        {
+            try
+            {
+                using (var context = new BirdTradingPlatformContext())
+                {
+                    var p1 = context.Categories.SingleOrDefault(p => p.CategoryId == Category.CategoryId);
+                    context.Categories.Remove(p1);
+                    context.SaveChanges();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
     }
 }
--- BirdTradingPlatformPRN231/DataAccess/ProductDAO.cs
+++ BirdTradingPlatformPRN231/DataAccess/ProductDAO.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using BusinessObject.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,89 @@ namespace DataAccess
 {
     public class ProductDAO
     {
+        public static List<Product> GetProducts()
+        {
+            var listProducts = new List<Product>();
+            try
+            {
+                using (var context = new BirdTradingPlatformContext())
+                {
+                    listProducts = context.Products
+                        .Include(c => c.Category)
+                        .Include(s => s.Store)
+                        .ToList();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            return listProducts;
+        }
+        public static Product FindProductById(int ProductId)
+        {
+            Product p = new Product();
+            try
+            {
+                using (var context = new BirdTradingPlatformContext())
+                {
+                    p = context.Products
+                        .Include(c => c.Category)
+                        .Include(s => s.Store)
+                        .SingleOrDefault(x => x.ProductId == ProductId);
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            return p;
+        }
+        public static void SaveProduct(Product Product)
+        {
+            try
+            {
+                using (var context = new BirdTradingPlatformContext())
+                {
+                    context.Products.Add(Product);
+                    context.SaveChanges();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+        public static void UpdateProduct(Product Product)
+        {
+            try
+            {
+                using (var context = new BirdTradingPlatformContext())
+                {
+                    context.Entry<Product>(Product).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+                    context.SaveChanges();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+        public static void DeleteProduct(Product Product)
+        {
+            try
+            {
+                using (var context = new BirdTradingPlatformContext())
+                {
+                    var p1 = context.Products.SingleOrDefault(p => p.ProductId == Product.ProductId);
+                    context.Products.Remove(p1);
+                    context.SaveChanges();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
     }
 }
