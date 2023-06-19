d3a1e0ea00f350c1dc8ef17896123bebc8b5f95e
DucLM42 <leduchien09@gmail.com>
2023-06-19 20:50:21 +0700

    Init ASP.NET MVC client project, test for authentication and authorization [DucLM]

---
+++ BirdTradingPlatformPRN231/BirdTradingPlatformClient/Controllers/HomeController.cs
@@ -0,0 +1,152 @@
+﻿using BirdTradingPlatformClient.Models;
+using BusinessObject.Common;
+using BusinessObject.DTOs;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Diagnostics;
+using System.Net.Http.Headers;
+using System.Text;
+using Microsoft.IdentityModel.Logging;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Configuration;
+using Microsoft.AspNetCore.Authorization;
+
+namespace BirdTradingPlatformClient.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly HttpClient client = null;
+        private string UserApilUrl = "";
+        private readonly IConfiguration _configuration;
+
+        public HomeController(IConfiguration configuration)
+        {
+            client = new HttpClient();
+            var contentType = new MediaTypeWithQualityHeaderValue("application/json");
+            client.DefaultRequestHeaders.Accept.Add(contentType);
+            UserApilUrl = "http://localhost:5208/api/User";
+            _configuration = configuration;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        [Authorize(Roles = "CUSTOMER")]
+        public IActionResult Customer()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        [Authorize(Roles = "STORE_STAFF")]
+        public IActionResult Staff()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Forbidden()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginDTO request)
+        {
+            // Validate on client side
+            if (!ModelState.IsValid)  return View(request);
+
+            // Initiate ApiResult obj
+            APIResult<string>? result;
+
+            // Post LoginDTO request
+            var jsonRequest = JsonConvert.SerializeObject(request);
+
+            HttpResponseMessage response = await client.PostAsync(UserApilUrl + "/Authenticate",
+                new StringContent(jsonRequest, Encoding.UTF8, "application/json"));
+            
+            // Get response
+            // If login failed
+            if (!response.IsSuccessStatusCode)
+            {
+                result = JsonConvert.DeserializeObject<APIErrorResult<string>>(await response.Content.ReadAsStringAsync());
+                ViewBag.ErrorMessage = result?.Message;
+                return View(request);
+            }
+
+            // If login success
+            result = JsonConvert.DeserializeObject<APISuccessResult<string>>(await response.Content.ReadAsStringAsync());
+            var userPrincipal = this.ValidateToken(result.ResultObj);
+            var authProperties = new AuthenticationProperties
+            {
+                ExpiresUtc = DateTimeOffset.UtcNow.AddMinutes(10),
+                IsPersistent = false
+            };
+
+            HttpContext.Session.SetString("Token", result.ResultObj);
+            await HttpContext.SignInAsync(
+                        CookieAuthenticationDefaults.AuthenticationScheme,
+                        userPrincipal,
+                        authProperties);
+
+            // Check for role authorization and redirection
+            if (userPrincipal.IsInRole("CUSTOMER"))
+            {
+                return RedirectToAction("Customer", "Home");
+            }
+
+            return RedirectToAction("Staff", "Home");
+        }
+
+        private ClaimsPrincipal ValidateToken(string jwtToken)
+        {
+            IdentityModelEventSource.ShowPII = true;
+
+            SecurityToken validatedToken;
+            TokenValidationParameters validationParameters = new TokenValidationParameters();
+
+            validationParameters.ValidateLifetime = true;
+
+            validationParameters.ValidAudience = _configuration["JWT:Issuer"];
+            validationParameters.ValidIssuer = _configuration["JWT:Issuer"];
+            validationParameters.IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["JWT:Secret"]));
+
+            ClaimsPrincipal principal = new JwtSecurityTokenHandler().ValidateToken(jwtToken, validationParameters, out validatedToken);
+
+            return principal;
+        }
+
+        [Authorize(Roles = "CUSTOMER, STORE_STAFF")]
+        public IActionResult Logout(string returnUrl)
+        {
+            // Remove Identity
+            HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+
+            // Back to home
+            if (string.IsNullOrEmpty(returnUrl))
+            {
+                return RedirectToAction("Index");
+            }
+            return Redirect(returnUrl);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ BirdTradingPlatformPRN231/BirdTradingPlatformClient/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BirdTradingPlatformClient.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
