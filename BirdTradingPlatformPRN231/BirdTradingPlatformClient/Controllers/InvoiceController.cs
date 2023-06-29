f19064ed4effd14b182bbc12087489dfab60a4d0
DucLM42 <leduchien09@gmail.com>
2023-06-29 16:29:51 +0700

    Update MVC Client's pages [DucLM]

---
+++ BirdTradingPlatformPRN231/BirdTradingPlatformClient/Controllers/InvoiceController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BirdTradingPlatformClient.Controllers
+{
+    public class InvoiceController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Detail()
+        {
+            return View();
+        }
+    }
+}
