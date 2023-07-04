6c8ba91b210e903da45b09548fbd5cc5b8937891
DucLM42 <leduchien09@gmail.com>
2023-07-04 21:51:34 +0700

    Add store profile view DTO [DucLM]

---
+++ BirdTradingPlatformPRN231/BusinessObject/DTOs/StoreProfileViewDTO.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessObject.DTOs
+{
+    public class StoreProfileViewDTO
+    {
+        public string Email { get; set; }
+        public string Name { get; set; }
+        public string Phone { get; set; }
+        public string StoreName { get; set; }
+        public string StoreAddress { get; set; }
+        public string StoreDescription { get; set; }
+        public string StoreLogoImage { get; set; }
+        public string StoreCoverImage { get; set; }
+    }
+}
